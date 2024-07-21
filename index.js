5337598fab31574ce29af5d58a8f033fbdd1651d
Maksym-Zyza <mmziza88@gmail.com>
2024-07-21 12:42:08 +0300

    added tasks 1-4

---
+++ index.js
@@ -0,0 +1,43 @@
+import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} from "./src/contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, ...data }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      return console.log(contacts);
+
+    case "get":
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await addContact(data);
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
